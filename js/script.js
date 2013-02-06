2d43aa797cce26ed9d079acc11b5ed36886c0735
OpenShift guest <blackmwana@gmail.com>
2013-02-06 15:31:44 -0500

    stackmob init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 //author= blackmwana
-$(document).ready(function(){
-    alert("we");
+$(document).ready(function() {
+    var to = StackMob.Model.extend({
+        schemaName: 'testobject'
     });
+    var myto = new to();
+    myto.fetch({
+        success: function(model) {
+            console.debug(model.toJSON());
+        },
+        error: function(model, response) {
+            console.debug(response);
+        }
+    });
+});
