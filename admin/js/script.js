99bcc28dae674e3c270546fa76ba33725570ce54
OpenShift guest <blackmwana@gmail.com>
2013-02-09 18:00:26 -0500

    form test

--- admin/js/script.js
+++ admin/js/script.js
@@ -6,6 +6,20 @@ $(document).ready(function() {
             
         },
         initialize: function(){
+            this.template = _.template($('#login-temp').html());
+            
+        },
+        render: function(){
+            return this;
+        }
+    });
+    
+    var AppView = Backbone.View.extend({
+        el:'body',
+        events:{
+            
+        },
+        initialize: function(){
             
         },
         render: function(){
--- js/stackmobinit.js
+++ js/stackmobinit.js
@@ -4,4 +4,4 @@
 				publicKey: "e3d6dd70-bd8f-4059-b6bf-0c1792f57771",
 				apiVersion: 0
 			});
-            alert("stackmob is ready baas");
+         //   alert("stackmob is ready baas");
