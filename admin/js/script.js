183d72ce5dc3e6be4b4934d72c96c2693ec86da4
blackmwana <blackmwana@gmail.com>
2013-03-06 18:58:04 -0500

    test

--- admin/js/script.js
+++ admin/js/script.js
@@ -680,7 +680,7 @@ $(document).ready(function() {
         id:"cat-content",
         className:"page-region-content ",
         events: {
-            "click tr":"showCatEdit"
+            "click tatr":"showCatEdit"
         },
         initialize: function() {
             this.template = _.template($('#item-cats').html());
